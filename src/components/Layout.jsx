42a43e543799597c842b17336cf81506bc6ef3de
Lee Jiyoung <lizzie.yyy@gmail.com>
2023-06-15 17:19:25 +0900

    docs: README.md, jsconfig.json, .prettierrc

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
 function Layout({ children }) {
-    return (
-        <div id="wrap">
-            <header>
-                <h1>My Todo List</h1>
-            </header>
+  return (
+    <div id="wrap">
+      <header>
+        <h1>My Todo List</h1>
+      </header>
 
-            {/* <div id="container"> */}
-            <main id="main">{children}</main>
-        </div>
-    );
+      {/* <div id="container"> */}
+      <main id="main">{children}</main>
+    </div>
+  );
 }
 
 export default Layout;
