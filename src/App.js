fe1748cf5df86ed9903e62979faee473e64a0e7f
Lee Jiyoung <lizzie.yyy@gmail.com>
2023-06-15 15:30:18 +0900

    design(*): Add markup and CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
+import './reset.css';
 import './App.css';
 
 function App() {
-    return <></>;
+    return (
+        <div id="wrap">
+            <header>
+                <h1>My Todo List</h1>
+            </header>
+
+            <main id="main">
+                <form className="add-form-box">
+                    <label>
+                        제목
+                        <input className="form" type="text"></input>
+                    </label>
+
+                    <label>
+                        내용
+                        <input className="form" type="text"></input>
+                    </label>
+
+                    <button>추가하기</button>
+                </form>
+
+                <section>
+                    <h2>Working.. 🔥</h2>
+                    <ul className="todo-list">
+                        <li>
+                            <strong>제목</strong>
+                            <p>내용</p>
+                            <div className="btn-wrap">
+                                <button className="btn-del">삭제하기</button>
+                                <button className="btn-done">완료</button>
+                            </div>
+                        </li>
+                    </ul>
+                </section>
+
+                <section>
+                    <h2>Done..! 🎉</h2>
+                    <ul className="todo-list done">
+                        <li>
+                            <strong>제목</strong>
+                            <p>내용</p>
+                            <div className="btn-wrap">
+                                <button className="btn-del">삭제하기</button>
+                                <button className="btn-done">완료</button>
+                            </div>
+                        </li>
+                    </ul>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
